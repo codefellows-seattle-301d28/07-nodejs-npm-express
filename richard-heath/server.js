33a38ddd310b2cd0db1a22329f7ec95c1acbe59b
heath smith <heathsmith73@gmail.com>
2017-12-06 10:48:19 -0800

    got the 404 to work along with the other pages

--- richard-heath/server.js
+++ richard-heath/server.js
@@ -6,17 +6,25 @@
 // setting up the PORT
 const express = require('express');
 const app = express();
-
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
 
-app.post('/articles', bodyParser, function(request, response) {
+app.use(express.static('./public'));
+//we are using express to organize our data for routes and view and such. we are pointing at ./public since that is where all of our data lives at.
+
+app.get('/new', (req, res) => {
+  res.sendFile('/public/new.html', {root: '.'});
+})
+
+app.post('/articles', bodyParser, (request, response) => {
   // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
   console.log(request.body);
   response.send('Record posted to server!!');
 })
 
-//we are using express to organize our data for routes and view and such. we are pointing at ./public since that is where all of our data lives at.
-app.use(express.static('./public'));
-
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+app.use((req, res) => {
+  res.status(404).send('Sorry can\'t find that!');
+  console.error('this is a test of the 404');
+});
