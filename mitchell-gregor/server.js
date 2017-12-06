aa6f7159773c7578857ada5048e0347ef8648693
Mitchell M <mitchellm1618@gmail.com>
2017-12-06 09:21:02 -0800

    began installation of express and added dependencies

--- mitchell-gregor/server.js
+++ mitchell-gregor/server.js
@@ -1,12 +1,16 @@
 'use strict';
 
 // REVIEW: There is a package here called body-parser, which is used by the provided POST route. Be sure to install that and save it as a dependency after you create your package.json.
+// DONE 1
 
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
+const express = require('express');
+let app = express();
+
 
 app.post('/articles', bodyParser, function(request, response) {
   // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
-  console.log(request.body);
+  console.log(request.bdy);
   response.send('Record posted to server!!');
-})
+});
