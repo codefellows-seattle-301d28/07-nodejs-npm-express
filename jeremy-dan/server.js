7b9e5ae93cf368093675034234748f4c8584fc0d
Jeremy <jpjazzy@hotmail.com>
2017-12-06 10:37:20 -0800

    added routes and html links

--- jeremy-dan/server.js
+++ jeremy-dan/server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// REVIEW: There is a package here called body-parser, which is used by the provided POST route. Be sure to install that and save it as a dependency after you create your package.json.
+// REVIEWED: There is a package here called body-parser, which is used by the provided POST route. Be sure to install that and save it as a dependency after you create your package.json.
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser').urlencoded({extended: true});
@@ -8,10 +8,17 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('./public')); //files are in the public directory to keep the tree organized in a standardized format. Express serves our local files by locating them in a static directory.
 
 app.post('/articles', bodyParser, function(request, response) {
-  // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
+  // REVIEWED: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
   console.log(request.body);
   response.send('Record posted to server!!');
 });
 
+app.get('/new', (req, res) => {
+  res.sendFile('new.html', {root: './public'});
+});
+
+app.get('/*', (req, res) => {
+  res.sendFile('404.jpg', {root: './public'});
+});
 
 app.listen(PORT, () => `Listening on ${PORT}`);
